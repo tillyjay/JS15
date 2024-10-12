6289f16fcc851a21e057a7d3bfb0a652778fa231
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-12 16:33:27 -0300

    - hardcocded select colour from array

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,8 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.getElementById('btn');
-const color = document.querySelector('color');
+const color = document.querySelector('.color');
 
 btn.addEventListener('click', function() {
+    
+    //get random number between 0 - 3 test
+    const randomNumber = 2;
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 
 });
