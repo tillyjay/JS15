58b2a97da7ead25451f97677dc096f735d264b8c
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-12 17:51:18 -0300

    - simple & hex code colour flipper complete

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -9,12 +9,14 @@ const color = document.querySelector('.color');
 //add event listener for button click 
 btn.addEventListener('click', () => {
 
-    //call function randomHexCode passing hex arrau vals to get random hex code
-    let randomHexCode = randomHex(hex);
-
+    //begining hash
+    const hash = "#"
+    //call function randomHexCode passing hex array vals to get random hex code
+    const hexMinusHash = randomHex(hex);
+    //combine # and hexCode to create full random hex code
+    const randomHexCode = hash + hexMinusHash;
     //set background colour of body to random colour
     document.body.style.backgroundColor = randomHexCode;
-
     //update text content of the 'color' span with chosen color
     color.textContent = randomHexCode;
 });
@@ -26,13 +28,11 @@ let randomHex = (hex) => {
     //hex code length
     const hexCodeLen = 6; 
     let hexCode = "";
-
     //loop through generating random element from hex array 6 times and added each ele to hexCode string
     for (let i = 0; i < hexCodeLen; i++) {
     const randomHexEl = hex[Math.floor(Math.random() * hex.length)];
         hexCode += randomHexEl;
     }
 
-    console.log(hexCode);
-
+    return hexCode;
 }
