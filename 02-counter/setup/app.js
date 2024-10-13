da10c28753ca9920cad8a71c2843bf344dfe6d7c
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-13 10:15:32 -0300

    - completed counter with colour changes to span value

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -1,2 +1,39 @@
 //set initial count 
 let count = 0;
+
+//select value and buttons 
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+//loop through nodr list, add event listner to each button on click
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+
+        //change span value by applying count depending on btn class
+        if (btn.classList.contains('decrease')) {
+            count -= 1;
+            value.textContent = count;
+        } else if (btn.classList.contains('increase')) {
+            count += 1;
+            value.textContent = count;
+        } else if (btn.classList.contains('reset')) {
+            count = 0;
+            value.textContent = count;
+        }
+
+        //change span value colour depending on above, below, or 0
+        if(count >= 1) {
+            value.style.color = 'green';
+        } else if (count <= -1) {
+            value.style.color = 'red';
+        } else {
+            value.style.color = 'black';
+        }
+        
+    });
+
+});
+
+
+
+
