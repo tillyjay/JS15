f55f185590a3e2b8593fef73a0b4b163e97de36e
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-14 13:53:52 -0300

    - completed links toggle functionality

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -1,5 +1,9 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+//select nav toggle and links
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+//add an event listener to navToggle button
+navToggle.addEventListener('click', () => {
+    //toggle 'show-links' class on links, will either add or remove the class
+    links.classList.toggle('show-links');
+});
