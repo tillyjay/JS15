f6e1a0369bf2c764b88e50d6864d30d3171f9f11
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-15 11:58:22 -0300

    - show/hide question text using query selctors and event listners

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,4 +1,17 @@
 //using selectors inside the element
+const question = document.querySelector('.question');
+const plusBtn = document.querySelector('.plus-icon');
+const minusBtn = document.querySelector('.minus-icon');
+
+//add event listner to show question text on plus button click 
+plusBtn.addEventListener('click', () => {
+    question.classList.toggle('show-text');
+});
+
+//add event listner to hide question text on minus button click
+minusBtn.addEventListener('click', () => {
+    question.classList.remove('show-text');
+});
 
 
 // traversing the dom
