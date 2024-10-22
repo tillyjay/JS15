3e1dbbb810558301abdba1a4a4031bbb8893e647
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-22 12:16:08 -0300

    - feat: set up dynamic date

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,8 +4,12 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+
+
 
 // ********** fixed navbar ************
 
