6be7d42deacbbc93f73af5a67eae3b3d39a110ec
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-28 12:00:28 -0300

    - feat: setup alert functionality

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -12,9 +12,45 @@ let editElement;
 let editFlag = false;
 let editID = '';
 
-// ****** EVENT LISTENERS **********
 
 // ****** FUNCTIONS **********
+const addItem = (e) => {
+    e.preventDefault();
+
+    const value = grocery.value;
+    const id = new Date ().getTime().toString();
+
+    if (value && !editFlag) {
+
+        console.log('add item to list');
+    } else if (value && editFlag) {
+        
+        console.log('editing');
+    } else {
+
+        displayAlert('please enter value', 'danger');
+    }
+};
+
+//display alert 
+const displayAlert = (text, action) => {
+    
+    //set message and make alert visible
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    //hide alert after 3 seconds 
+    setTimeout(() => {
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`);
+    }, 3000);
+};
+
+
+// ****** EVENT LISTENERS **********
+//submit form 
+form.addEventListener('submit', addItem);
+
 
 // ****** LOCAL STORAGE **********
 
